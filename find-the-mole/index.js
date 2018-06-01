ab4debf02ba9c4e520ccc2a4587ca84153993014
Steffany <kielen@gmail.com>
2018-06-01 11:18:54 -0700

    added reset and point functions

--- find-the-mole/index.js
+++ find-the-mole/index.js
@@ -1,5 +1,27 @@
 $(document).ready(initializeApp);
 
 function initializeApp() {
+  $('.reset-button').click(resetGame);
   console.log("works!");
+}
+
+function dogePoint(){
+  var pointCounter = 0;
+  $('.clickableDoge').click(function(){
+    pointCounter++;
+    clickSound.play();
+    $('.score').text(pointCounter);
+  })
+  return pointCounter;
+}
+
+
+
+
+function resetGame(pointCounter){
+  var pointCounter = 0;
+}
+
+var sounds = {
+  clickSound: new Audio(click.wav),
 }
