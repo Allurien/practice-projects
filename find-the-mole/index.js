5d9c32d54998590f88f594fa4e934636ecf7cbe7
delray <d.a.faskey@gmail.com>
2018-06-01 11:21:13 -0700

    created and attached click handler for dirtHill class

--- find-the-mole/index.js
+++ find-the-mole/index.js
@@ -1,5 +1,17 @@
 $(document).ready(initializeApp);
 
 function initializeApp() {
+  attachClickHandlers();
   console.log("works!");
 }
+
+function attachClickHandlers(){
+  $(".dirtHill").on("click", dogeClickHandler);
+};
+
+function dogeClickHandler(){
+  if ($(this).hasClass(".dogeImg")){
+    $(this).removeClass(".dogeImg");
+    pointCounter +=1;
+  };
+};
